039dfbe7485f3dc4f5cdd04a85cda929d3d8171f
Andrew McKinney <andrewryanmckinney@gmail.com>
2020-03-24 22:15:40 -0400

    init of data api

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 var myMap = L.map('map', {
-    center: [40.7128, -74.0059],
+    center: [36.16273,-86.7816],
     zoom: 11
 })
 
@@ -10,8 +10,14 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
-// var dataLink = 'https://data-beta-nyc-files.s3.amazonaws.com/resources/35dd04fb-81b3-479b-a074-a27a37888ce7/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson?Signature=zCGxORklgA38BJiyT1NNWySfQG8%3D&Expires=1584488534&AWSAccessKeyId=AKIAWM5UKMRH2KITC3QA'
+var limit = 10000
 
-var dataRaw = 'static/data/nyc.geojson'
+url = `https://data.nashville.gov/resource/3wb6-xy3j.geojson`
 
-d3.json(dataRaw, (data) => L.geoJson(data).addTo(myMap))
+
+d3.json(url, (data) => {
+  console.log(data)
+  L.geoJson(data).addTo(myMap)
+
+
+})
