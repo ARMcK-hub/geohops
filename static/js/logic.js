b87e3bae6b87e0a526557af6c2ec2d3952449d3c
Andrew McKinney <andrewryanmckinney@gmail.com>
2020-03-26 22:48:43 -0400

    added date filter and started d3 event listeners

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,54 @@
+// GeoHops
+// Creation Date: 2020-03-24
+// Authors: Drew McKinney, Nick Riff, Jon Disarufino, Karsten Olson
+
+
+// DEVELOPER TOOLS
+var limit = 10000
+var url = `https://data.nashville.gov/resource/3wb6-xy3j.geojson`
+
+
+
+//////////////////////
+//    Functions     //
+//////////////////////
+
+// Filtering Dataset based on Event Listener Dates
+function filterDate(features, startDate, endDate) {
+  var features = features
+  var filteredFeatures = []
+  var startDate = Date.parse(startDate)
+  var endDate = Date.parse(endDate)
+
+  features.forEach( (feature) => {
+
+    var issuedDate = Date.parse(String(feature.properties.date_issued).split("T")[0])
+
+    if (issuedDate) {
+      if (issuedDate >= startDate && issuedDate <= endDate) {filteredFeatures.push(feature)}
+    }
+  })
+
+  return filteredFeatures
+
+}
+
+
+
+//////////////////////
+//    D3 OBJECTS    //
+//////////////////////
+var buttonFilter = d3.select('#filter-btn')
+
+
+
+// Defining myMap Object
 var myMap = L.map('map', {
     center: [36.16273,-86.7816],
     zoom: 13
 })
 
+// Generating Map from MapBox for myMap
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
@@ -10,20 +56,32 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
-var limit = 10000
-
-url = `https://data.nashville.gov/resource/3wb6-xy3j.geojson`
 
 
+
+
+// Requesting geojson API call
 d3.json(url, (data) => {
   console.log(data)
 
-// Date lookup: data.properties.date_issued (YYYY-MM-DDT00:00:00.000)
+  // Date lookup: data.properties.date_issued (YYYY-MM-DDT00:00:00.000)
 
-  features = data.features
+
+  var features = data.features
   var arrHeat = []
+  var startDate = "1-1-2015"
+  var endDate = "1-1-2020"
 
-  features.forEach(feature => {
+  // Filtering Data on Date
+  var filteredFeatures = filterDate(features, startDate, endDate)
+  
+
+  //////////////////////
+  //     HEAT MAP     //
+  //////////////////////
+
+  // Creating Location Array for Heat Map
+  filteredFeatures.forEach(feature => {
     if (feature.geometry !== null) {
 
       var lat = feature.geometry.coordinates[1]
@@ -34,9 +92,9 @@ d3.json(url, (data) => {
       }
   })
 
-  console.log(arrHeat)
+  console.log("Heat Map Data Count: " + arrHeat.length)
 
-  
+  // Plotting Heatmap
   var mapHeat = L.heatLayer(arrHeat, {
       radius: 25,
       blur: 20,
