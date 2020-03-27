6476d6b1845c5cff158f611e0c53f5412ebdc48b
Andrew McKinney <andrewryanmckinney@gmail.com>
2020-03-26 20:30:06 -0400

    created basic heatmap

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,6 @@
 var myMap = L.map('map', {
     center: [36.16273,-86.7816],
-    zoom: 11
+    zoom: 13
 })
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -17,7 +17,33 @@ url = `https://data.nashville.gov/resource/3wb6-xy3j.geojson`
 
 d3.json(url, (data) => {
   console.log(data)
-  L.geoJson(data).addTo(myMap)
+
+// Date lookup: data.properties.date_issued (YYYY-MM-DDT00:00:00.000)
+
+  features = data.features
+  var arrHeat = []
+
+  features.forEach(feature => {
+    if (feature.geometry !== null) {
+
+      var lat = feature.geometry.coordinates[1]
+      var lng = feature.geometry.coordinates[0]
+
+      console.log(lat, lng)
+      var location = [lat, lng]
+      
+      if (lat && lng) { arrHeat.push(location) }
+
+      }
+  })
+
+  var mapHeat = L.heatLayer(arrHeat, {
+      radius: 25,
+      blur: 20,
+      maxZoom: 15
+      
+  }).addTo(myMap)
+
 
 
 })
