33afc3480a84b98fb246a1ac636f2f518aacaa1d
Andrew McKinney <andrewryanmckinney@gmail.com>
2020-03-28 13:50:52 -0400

    integrated pie and bar charts into init, increased call limit

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,7 +5,7 @@
 
 // DEVELOPER TOOLS
 var limit = 10000
-var url = `https://data.nashville.gov/resource/3wb6-xy3j.geojson`
+var url = `https://data.nashville.gov/resource/3wb6-xy3j.geojson?$limit=${limit}`
 
 
 
@@ -32,6 +32,29 @@ function filterDate(features, startDate, endDate) {
   return filteredFeatures
 
 }
+
+// Field Count for different fields in feature set
+function fieldCount(features, field) {
+  featureCount = {}
+  features.forEach( feature => {
+    type = feature.properties[field]
+    featureCount[type] = (featureCount[type]) ? featureCount[type] + 1 : 1
+  })
+  return featureCount
+}
+
+function yearCount(features, field) {
+  featureCount = {}
+
+  features.forEach( feature => {
+    if (feature.properties[field]) {
+    type = feature.properties[field].split("-")[0]
+    featureCount[type] = (featureCount[type]) ? featureCount[type] + 1 : 1
+    }
+  })
+  return featureCount
+}
+
 
 
 
@@ -100,7 +123,102 @@ d3.json(url, (data) => {
     maxZoom: 15
   }).addTo(myMap)
 
-  // @TODO: insert Plotly initial plots
+
+
+  //////////////////////
+  //     Bar Chart    //
+  //////////////////////
+    
+    // console.log(yearCount(filteredFeatures, 'date_issued'))
+    yearCounts = yearCount(filteredFeatures, 'date_issued')
+    yearLabels = Object.keys(yearCounts)
+    yearValue = Object.values(yearCounts)
+    // console.log(yearLabels, yearValue)
+  
+    //  Create the Traces
+    var trace1 = {
+      x: yearLabels,
+      y: yearValue,
+      type: "bar",
+    };
+  
+    // Create the data array for the plot
+    var data = [trace1];
+  
+    // Define the plot layout
+    var layout = {
+      title: "Number Of Beer Permits",
+      xaxis: { title: "Date" },
+      yaxis: { title: "Number of Permits" }
+    };
+  
+    // Plot the chart to a div tag with id "plot"
+    Plotly.newPlot("plot", data, layout);
+
+
+
+
+
+
+
+  //////////////////////
+  //     Pie Chart    //
+  //////////////////////
+
+
+  // On Sales///
+  function onSelect(data) {
+    return data.properties.permit_subtype === "ONSALES";
+}
+var onSalesPermit = filteredFeatures.filter(onSelect);
+var onSalesCount = onSalesPermit.length; 
+console.log(onSalesCount)
+// Off Sales////
+function offSelect(data) {
+    return data.properties.permit_subtype === "OFFSALES";
+}
+var offSalesPermit = filteredFeatures.filter(offSelect);
+var offSalesCount = offSalesPermit.length; 
+console.log(offSalesCount)
+// On Off Sales///
+function onOffSelect(data) {
+    return data.properties.permit_subtype === "ONOFFSALES";
+}
+var onOffSalesPermit = filteredFeatures.filter(onOffSelect);
+var onOffSalesCount = onOffSalesPermit.length; 
+console.log(onOffSalesCount)
+// SPECIAL/////
+function specialSelect(data) {
+    return data.properties.permit_subtype === "SPECIAL";
+}
+var specialSalesPermit = filteredFeatures.filter(specialSelect);
+var specialSalesCount = specialSalesPermit.length; 
+console.log(specialSalesCount)
+
+// WHOLESALE/////
+function wholeSaleSelect(data) {
+    return data.properties.permit_subtype === "WHOLESALES";
+}
+var wholeSalesPermit = filteredFeatures.filter(wholeSaleSelect);
+var wholeSalesCount = wholeSalesPermit.length; 
+console.log(wholeSalesCount)
+// bar chart////
+var trace1 = {
+    labels: ["On Sales", "Off Sales", "On & Off Sales", "Special Sales", "Whole Sales"],
+    values: [onSalesCount, offSalesCount, onOffSalesCount, specialSalesCount, wholeSalesCount],
+    type: 'pie',
+    textinfo: "label+percent",
+    insidetextorientation: "radial"
+  };
+  
+  var data = [trace1];
+  
+  var layout = {
+    title: "Liquor Licence By Permit Type",
+  };
+  
+  Plotly.newPlot("pie", data, layout);
+
 
 
   
@@ -154,8 +272,8 @@ buttonFilter.on('click', function() {
     }).addTo(myMap)
 
     // @TODO: Inersert Plotly plots update on filter
-
-
+    console.log(yearCount(filteredFeatures, 'date_issued'))
+    
 
 
   })
